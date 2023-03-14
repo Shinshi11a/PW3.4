c21e98fd4e2539ddb0c38ee718314e3ae215057e
Shinshi11a <pro100vanyok2016@gmail.com>
2023-03-14 19:47:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿class MyIntList
+{
+    private List<int> numbersList = new List<int>();
+
+    public double Average
+    {
+        get
+        {
+            return CalculateAverage();
+        }
+    }
+
+    public int MinValue
+    {
+        get
+        {
+            return numbersList.Min();
+        }
+    }
+
+    public int MaxValue
+    {
+        get
+        {
+            return numbersList.Max();
+        }
+    }
+
+    public void AddNumber(int number)
+    {
+        numbersList.Add(number);
+    }
+
+    public void RemoveNumber(int number)
+    {
+        numbersList.Remove(number);
+    }
+
+    public double CalculateAverage()
+    {
+        int sum = 0;
+        foreach (int number in numbersList)
+        {
+            sum += number;
+        }
+        return sum / (double)numbersList.Count;
+    }
+}
